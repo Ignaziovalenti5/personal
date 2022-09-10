1ad6c3a85b5eeed029c5eff21f44826c3992ad4f
Ignaziovalenti5 <ignazio.valenti.87@gmail.com>
2022-09-10 21:49:53 +0200

    bugs fixed

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+let main = document.querySelector('.header-container')
+let social = document.querySelector('#social')
+
+console.log(main.childNodes);
+console.log(social.childNodes);
+
+for(let c of main.childNodes) {
+  console.log(c);
+}
